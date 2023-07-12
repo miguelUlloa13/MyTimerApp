d9923914ae4f0a6c929781c4af620bd06200fe87
Miguel Ulloa <miguel.bmangelulloa13@gmail.com>
2023-07-12 17:02:36 -0600

    FeatV0.1
    Structuring the project

---
+++ MyTimerApp/MyTimerApp/Coordinator/MainCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  MainCoordinator.swift
+//  MyTimerApp
+//
+//  Created by Miguel Angel Bric Ulloa on 12/07/23.
+//
+
+import Foundation
---
+++ MyTimerApp/MyTimerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTimerApp
+//
+//  Created by Miguel Angel Bric Ulloa on 12/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
