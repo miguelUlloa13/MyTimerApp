364c7e11608ca283945de2c376df31aa3c1f33a1
Miguel Ulloa <miguel.bmangelulloa13@gmail.com>
2023-07-13 00:46:34 -0600

    Feat0.2
    Menu

---
+++ MyTimerApp/MyTimerApp/Menu/Model/MenuModel.swift
@@ -0,0 +1,38 @@
+//
+//  MenuDataModel.swift
+//  MyTimerApp
+//
+//  Created by Miguel Angel Bric Ulloa on 12/07/23.
+//
+
+import Foundation
+import UIKit
+
+class MenuDataModel{
+    
+    func showMenu(){
+        /*
+        myView.layer.cornerRadius = 40
+        myBGImage.layer.cornerRadius = myView.layer.cornerRadius
+        
+        let x = screen.width * 0.8
+        let originalTransform = myView.transform
+        let scaledTransform = originalTransform.scaledBy(x: 0.85, y: 0.85)
+        let scaledAndTranslatedTransform = scaledTransform.translatedBy(x: x, y: 50)
+        UIView.animate(withDuration: 0.7) {
+            self.myView.transform = scaledAndTranslatedTransform
+            
+        }
+         */
+    }
+    
+    func closeMenu() {
+        /*
+        UIView.animate(withDuration: 0.7) {
+            self.myView.transform = self.firstViewPosition
+            self.myView.layer.cornerRadius = 0
+            self.myBGImage.layer.cornerRadius = self.myView.layer.cornerRadius
+        }
+         */
+    }
+}
